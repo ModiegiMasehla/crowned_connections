8176d8d3d3b7c935ea2c1063cabcfac8be0e7ddb
Modiegi Masehla <modiegiwamasehla@gmail.com>
2026-09-14 11:12:16 +0200

    add chat message to models

---
+++ lib/models/chat_message.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/// Represents a single message inside chats/{bookingId}/messages/{messageId}.
+class ChatMessage {
+  final String id;
+  final String senderId;
+  final String senderName;
+  final String text;
+  final DateTime timestamp;
+
+  ChatMessage({
+    required this.id,
+    required this.senderId,
+    required this.senderName,
+    required this.text,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'senderId': senderId,
+      'senderName': senderName,
+      'text': text,
+      'timestamp': Timestamp.fromDate(timestamp),
+    };
+  }
+
+  factory ChatMessage.fromMap(String id, Map<String, dynamic> map) {
+    return ChatMessage(
+      id: id,
+      senderId: map['senderId'] ?? '',
+      senderName: map['senderName'] ?? '',
+      text: map['text'] ?? '',
+      timestamp: (map['timestamp'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+}
